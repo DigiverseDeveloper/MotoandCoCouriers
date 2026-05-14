a31c2bfec985b26cc2728f28e69364219a7ac93c
DigiverseDeveloper <josephine@otimi.com.au>
2026-05-14 19:04:24 +1000

    Load client order date cleanup bridge

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import MotoCoLogistics from './MotoCoLogistics.jsx';
 import SecureLoginBridge from './SecureLoginBridge.jsx';
 import ZohoDealBridge from './ZohoDealBridge.jsx';
 import DriverWorkflowBridge from './DriverWorkflowBridge.jsx';
+import ClientOrderDateBridge from './ClientOrderDateBridge.jsx';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <ZohoDealBridge>
         <MotoCoLogistics />
         <DriverWorkflowBridge />
+        <ClientOrderDateBridge />
       </ZohoDealBridge>
     </SecureLoginBridge>
   );
